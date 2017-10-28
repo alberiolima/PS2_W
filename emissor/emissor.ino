88edf79a62d2004cd4f149fa3643ae974beabec6
Alberio Lima <alberio@dadox.com.br>
2017-10-27 23:29:06 -0300

    Adicionados novos exemplos

--- emissor/emissor.ino
+++ emissor/emissor.ino
@@ -1,5 +1,13 @@
-/* Connections:
+/* 
+ *  Módumo emissor para controle remoto sem fio com base no PS2 e Arduino
+ *  Francisco Albério Lima - Brasil
+ *  
+ *  Repositório: https://github.com/alberiolima/PS2_W
+ *  
  
+ 
+ 
+ Connections: 
  Arduino -> LT8900
 
  GND        GND
@@ -17,44 +25,26 @@
 #include <SPI.h>
 #include "LT8900.h"
 
-#define PS2_DAT        6 //Marrom   (usar pull-up)
-#define PS2_CMD        4 //Laranja
-#define PS2_SEL        3 //Amarelho (usar pull-up)
-#define PS2_CLK        5 //Azul
-//VCC 3.3V   (5V?)        //Vermelho
-//GND/Terra               //Preto
+//Pinos do arduino para a placa mãe do controle PS2
+#define PS2_DAT      6 //Marrom  
+#define PS2_CMD      4 //Laranja
+#define PS2_SEL      3 //Amarelho
+#define PS2_CLK      5 //Azul
 
-/*
-  Selecione os modos do controle:
-    - pressures = lendo botões do controle analógico
-    - rumble    = motor de vibração
-*/
-
-#define pressures   false //true
-#define rumble      false //true
-
-/*
-  Atualmente não é suportado que o controle seja plugado depois do arduino ter inicializado, então
-  Sempre que plugar o controle será necessário reiniciar o arduino ou charmar a função config_gamepad(pins)
-*/
-
-int error = 0;
-byte type = 0;
-byte vibrate = 0;
-
-const uint8_t PIN_NRF_RST = 10;
-const uint8_t PIN_NRF_CS  =  9;
-const uint8_t PIN_NRF_PKT =  8;
+//Pinos do arduino para o LT8900
+#define PIN_NRF_RST 10
+#define PIN_NRF_CS   9
+#define PIN_NRF_PKT  8
 
 LT8900 lt(PIN_NRF_CS, PIN_NRF_PKT, PIN_NRF_RST);
 
 PS2X ps2x; // Cria a Instancia da classe 
 
+void setup() {
+  
+  //Serial.begin(9600); //Descomente para depurar
 
-void setup()
-{
-  Serial.begin(9600);
-
+  //Inicializa protocolo SPI que será utlizado pelo módulo LT8900
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE1);
@@ -62,13 +52,13 @@ void setup()
 
   delay(500);
 
+  //Inicializa módulo LT8900
   lt.begin();
   lt.setCurrentControl(4,15);
   lt.setDataRate(LT8900::LT8910_62KBPS);
   lt.setChannel(0x06);
 
-  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
-  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+  int error = ps2x.config_gamepad( PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT);
   
   if(error == 0){
     Serial.println("Controle encontrado, configurado com successo");
@@ -76,25 +66,25 @@ void setup()
     Serial.println("Erro no controle PS2");
   }
   
-
 }
 
-void loop()
-{
-    ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
+void loop(){
+  //Ler dados da placa-mãe de controle do PS2
+  ps2x.read_gamepad(); //read controller and set large motor to spin at 'vibrate' speed
     
-    //envio de dados
-    uint8_t data[21];
-    
-    for (byte i = 0; i< 22;i++){
-      data[i] = ps2x.Analog(i);
-      //Descomente para depurar
-      //Serial.print(data[i]);
-      //Serial.print( " " );
-    }
-    Serial.println();
+  //envio de dados
 
-    lt.sendPacket(data, 21);
-        
-    delay(50);
+  /* descomente para depurar
+  for (byte i = 0; i< 22;i++){
+    //Descomente para depurar
+    Serial.print(ps2x.PS2data[i]);
+    Serial.print( " " );
+  }
+  Serial.println();
+  */
+
+  lt.sendPacket(ps2x.PS2data, 9); //Envia apenas 9 bytes pois "pressure", que é a sensibilidade das teclas não está sendo ativada, 
+                                  //acreidto que na proxima versão utlize as posições de 1 a 3 para identificacão do controle
+  
+  delay(50);
 }
---
+++ rx_controle_servos_PS2_W/rx_controle_servos_PS2_W.ino
@@ -0,0 +1,107 @@
+/*
+ * 
+ * 
+ * Repositório em:https://github.com/alberiolima/PS2_W
+ * 
+ */
+
+#include <PS2X_lib.h> 
+#include <SPI.h>
+#include "LT8900.h"
+#include <Servo.h>
+
+const uint8_t PIN_NRF_RST = 10;
+const uint8_t PIN_NRF_CS  =  9;
+const uint8_t PIN_NRF_PKT =  8;
+
+#define pinoSERVO1 3
+#define pinoSERVO2 5
+
+int iPosiSERVO1 = 90;
+int iPosiSERVO2 = 90;
+
+#define servoInc1 5
+#define servoInc2 5
+
+Servo servo1; 
+Servo servo2; 
+
+LT8900 lt(PIN_NRF_CS, PIN_NRF_PKT, PIN_NRF_RST);
+
+PS2X ps2x; 
+
+void setup(){
+
+
+  servo1.attach(3);
+  servo2.attach(5);
+  
+  Serial.begin(9600);
+
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE1);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+
+  delay(500);
+
+  lt.begin();
+  lt.setCurrentControl(4,15);
+  lt.setDataRate(LT8900::LT8910_62KBPS);
+  lt.setChannel(0x06);
+
+  lt.startListening();  
+
+}
+
+void loop() {
+  
+    if (lt.available())  {
+
+      //Ler dados do módulo RF
+      uint8_t buf[32];
+      int packetSize = lt.read(buf, 32);
+      if (packetSize > 0)  {
+        for(int i = 0; i < packetSize; i++) {
+          ps2x.PS2data[i] = buf[i];         
+        }
+        ps2x.last_buttons = ps2x.buttons; //store the previous buttons states
+        ps2x.buttons = *(uint16_t*)(ps2x.PS2data+3);   //store as one value for multiple functions 
+      }     
+
+      lt.startListening();
+      
+      if ( ps2x.Analog(PSS_RY)  < 10 ) {
+        iPosiSERVO1 += servoInc1; 
+      } else if ( ps2x.Analog(PSS_RY) > 240  ) {
+        iPosiSERVO1 -= servoInc1; 
+      }
+      
+      if ( iPosiSERVO1 > 179 ) { 
+        iPosiSERVO1 = 179;
+      }
+      if ( iPosiSERVO1 < 0 ) { 
+        iPosiSERVO1 = 0;
+      }
+
+      if ( ps2x.Analog(PSS_RX)  < 10 ) {
+        iPosiSERVO2 += servoInc2; 
+      } else if ( ps2x.Analog(PSS_RX) > 240  ) {
+        iPosiSERVO2 -= servoInc2;
+      }
+      if ( iPosiSERVO2 > 179 ) { 
+        iPosiSERVO2 = 179;
+      }
+      if ( iPosiSERVO2 < 0 ) { 
+        iPosiSERVO2 = 0;
+      }
+
+      servo1.write( iPosiSERVO1 );
+      servo2.write( iPosiSERVO2 );
+      
+      Serial.print( iPosiSERVO1 );
+      Serial.print( " " );
+      Serial.print( iPosiSERVO2 );
+      Serial.println();
+    }  
+}
